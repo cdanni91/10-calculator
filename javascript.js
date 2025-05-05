ddaf809d027559a49a9046a72e490b4d442aaeb0
cdanni91 <cdanni91@gmail.com>
2025-05-05 13:30:53 -0300

    Add floating point feature

--- javascript.js
+++ javascript.js
@@ -1,29 +1,21 @@
-
-
 // what the user presses in the calculator
 let selection;
 
-buttons = ["1","2","3","/","4","5","6","*","1","2","3","-","0",".","=","+"];
+buttons = ["1","2","3","/","4","5","6","*","7","8","9","-","0",".","=","+"];
 
 function createGrid(size) {
-
     const buttonGrid = document.querySelector('.button-grid');
     buttonGrid.style.setProperty('--cols', size); 
     
-
     let previousSelection = "";
     let text = "";
     let firstNumber = "";
     let secondNumber = "";
     let currentOperator = "";
     let operatorCounter = 0;
-
-
-
-    
+    let isFloatPoint = false; // Track decimal point usage
 
     for (let i = 0; i < size * size; i++) {
-
         const calcButton = document.createElement('div');
         calcButton.innerText = buttons[i];
 
@@ -31,89 +23,72 @@ function createGrid(size) {
             const selection = buttons[i];
             const displayElement = document.querySelector(".display");
 
-
             switch (selection) {
-                    case "+":
-                    case "-":
-                    case "*":
-                    case "/":
-
-                        operatorCounter += 1;
-                    
-                        // if its the second time an operator was pressed
-                        if (operatorCounter > 1) {
-                            // the second number is saved 
-                            secondNumber = previousSelection;
-                    
-                            // performs the calculation using the first operator that was selected
-                            let result;
-
-                            switch (currentOperator) {
-                                case "+": result = +firstNumber + +secondNumber; break;
-                                case "-": result = +firstNumber - +secondNumber; break;
-                                case "*": result = +firstNumber * +secondNumber; break;
-                                case "/": result = +secondNumber !== 0 ? +firstNumber / +secondNumber : "Error"; break;
-                                default: result = +secondNumber;
-                            }
-                    
-                            // shows the result in the display
-                            displayElement.innerText = result;
-                            // the first number is replaced as the result
-                            firstNumber = result;
-                            // cleans the display and the selection
-                            previousSelection = "";
-                            text = "";
-                            // it saves the second selected operator
-                            currentOperator = selection;
-                            // sets the counter to 1
-                            operatorCounter = 1;
-                    
-                        } else {
-                            firstNumber = previousSelection;
-                            currentOperator = selection;
-                            previousSelection = "";
-                            text = "";
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    operatorCounter += 1;
+                    if (operatorCounter > 1) {
+                        secondNumber = previousSelection;
+                        let result;
+                        switch (currentOperator) {
+                            case "+": result = +firstNumber + +secondNumber; break;
+                            case "-": result = +firstNumber - +secondNumber; break;
+                            case "*": result = +firstNumber * +secondNumber; break;
+                            case "/": result = +secondNumber !== 0 ? +firstNumber / +secondNumber : "Error"; break;
+                            default: result = +secondNumber;
                         }
-                        break;
+                        displayElement.innerText = result;
+                        firstNumber = result;
+                        previousSelection = "";
+                        text = "";
+                        currentOperator = selection;
+                        operatorCounter = 1;
+                        isFloatPoint = false; // Reset for new number
+                    } else {
+                        firstNumber = previousSelection;
+                        currentOperator = selection;
+                        previousSelection = "";
+                        text = "";
+                        isFloatPoint = false; // Reset for new number
+                    }
+                    break;
 
+                case "=":
+                    let result;
+                    secondNumber = previousSelection;
+                    switch (currentOperator) {
+                        case "+": result = +firstNumber + +secondNumber; break;
+                        case "-": result = +firstNumber - +secondNumber; break;
+                        case "*": result = +firstNumber * +secondNumber; break;
+                        case "/": result = +secondNumber !== 0 ? +firstNumber / +secondNumber : "Noooooo papucho nooo"; break;
+                        default: result = +previousSelection;
+                    }
+                    displayElement.innerText = result;
+                    firstNumber = result;
+                    previousSelection = result;
+                    currentOperator = "";
+                    operatorCounter = 0;
+                    isFloatPoint = previousSelection.includes('.'); // Update based on result
+                    break;
 
-                        // if the user presses the = button
-                        case "=":
-                            let result;
-                            secondNumber = previousSelection;
-                            switch (currentOperator) {
-                                case "+": result = +firstNumber + +secondNumber; break;
-                                case "-": result = +firstNumber - +secondNumber; break;
-                                case "*": result = +firstNumber * +secondNumber; break;
-                                case "/": result = +secondNumber !== 0 ? +firstNumber / +secondNumber : "Noooooo papucho nooo"; break;
-                                default: result = +previousSelection;
-                            }
-                            displayElement.innerText = result;
-                            firstNumber = result;
-                            // save the result for further calculations
-                            previousSelection = result;
-                            // resets the operator parts
-                            currentOperator = "";
-                            operatorCounter = 0;
-                            break;
-
-                
-
-                
                 default:
-                    
+                    if (selection === ".") {
+                        if (isFloatPoint) break; // Ignore if already has decimal
+                        isFloatPoint = true;
+                    }
                     text = previousSelection + selection;
                     displayElement.innerText = text;
                     previousSelection = text;
                     break;
             }
-
         });
 
         buttonGrid.appendChild(calcButton);
     }
 
-    // clear button functionality
+    // Clear button
     const clearButton = document.querySelector(".clear");
     clearButton.addEventListener("click", () => {
         document.querySelector(".display").innerText = "";
@@ -121,21 +96,12 @@ function createGrid(size) {
         firstNumber = "";
         secondNumber = "";
         currentOperator = "";
-        operatorCounter = "";
-        
+        operatorCounter = 0;
+        isFloatPoint = false; // Reset on clear
     });
+}
 
-
-    }
-
-    
 createGrid(4);
-
-
-
-
-
-
 
 
 /* function operate(firstNumber,operator,secondNumber) {
